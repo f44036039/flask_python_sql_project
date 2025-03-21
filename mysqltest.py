49f4ffe3d9e7bd995813998f3a11cb1487925bb8
steven <f44036039@gs.ncku.edu.tw>
2025-03-22 00:03:55 +0800

    removed mysqltest.py

--- mysqltest.py
+++
@@ -1,9 +0,0 @@
-import pymysql # type: ignore
-con = pymysql.connect(
-    user = "root",
-    password = "12345678",
-    host = "localhost",
-    database = "mydb",
-)
-print("success")
-con.close()
